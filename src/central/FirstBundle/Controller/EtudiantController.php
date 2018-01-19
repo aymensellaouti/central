caaa0a1fdc1514ed17671843cee7069850246dfa
aymen <aymen.sellaouti@gmail.com>
2018-01-19 16:08:18 +0100

    update et delete Personne

--- src/central/FirstBundle/Controller/EtudiantController.php
+++ src/central/FirstBundle/Controller/EtudiantController.php
@@ -5,14 +5,23 @@ namespace central\FirstBundle\Controller;
 use central\FirstBundle\Entity\Etudiant;
 use central\FirstBundle\Form\EtudiantType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class EtudiantController extends Controller
 {
-    public function addAction()
+    public function addAction(Request $request)
     {
         $etudiant = new Etudiant();
         $form = $this->createForm(EtudiantType::class,$etudiant);
-
+        $form->handleRequest($request);
+        if($form->isSubmitted()&&$form->isValid()){
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($etudiant);
+            $em->flush();
+            return $this->render('@centralFirst/Etudiant/list.html.twig',array(
+                'etudiant'=>$etudiant
+            ));
+        }else
         return $this->render('@centralFirst/Etudiant/addEtudiant.html.twig', array(
             'form' => $form->createView())
         );
--- src/central/FirstBundle/Form/EtudiantType.php
+++ src/central/FirstBundle/Form/EtudiantType.php
@@ -7,6 +7,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\SubmitButton;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class EtudiantType extends AbstractType
